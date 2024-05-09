67136cfd1638da95f22f8f1577faebc7b64b8b29
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-09 09:25:12 -0400

    Create UI

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+//Create element for screen
+const screen = document.querySelector(".screen");
 
+function add(a, b){
+    screen.textContent = a + b;
+}
+
+function subtract(a, b){
+    screen.textContent = a - b;
+}
+
+function multiply(a, b){
+    screen.textContent = a * b;
+}
+
+function divide(a, b){
+    if (b==0){ screen.textContent = "You're kidding right?"
+} else {
+    screen.textContent = a / b;
+}
+}
