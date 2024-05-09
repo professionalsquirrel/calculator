630f1b4e661d37656a89b1afa4ec1fb574bcb56d
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-09 09:53:15 -0400

    Add some functions

--- script.js
+++ script.js
@@ -2,20 +2,45 @@
 const screen = document.querySelector(".screen");
 
 function add(a, b){
-    screen.textContent = a + b;
+    return a + b;
 }
 
 function subtract(a, b){
-    screen.textContent = a - b;
+    return a - b;
 }
 
 function multiply(a, b){
-    screen.textContent = a * b;
+    return a * b;
 }
 
 function divide(a, b){
     if (b==0){ screen.textContent = "You're kidding right?"
 } else {
-    screen.textContent = a / b;
+    return a / b;
 }
+}
+
+let operand1;
+let operator;
+let operand2;
+
+function operate(operator, a, b){
+    switch(operator){
+    case "+":
+        add(a, b);
+        break;
+    case "-":
+        subtract(a,b);
+        break;
+    case "*":
+        multiply(a,b);
+        break;
+    case "/":
+        divide(a,b);
+        break;
+    }
+}
+
+function populateDisplay(){
+
 }
